2ac69735a1a34db14e0c035b3e30f806f9423e25
Jhonatan <jhostia@unicesar.edu.co>
2023-09-20 09:48:19 -0500

    Actualizo la capa de entidades y creo una clase llamada cuota

---
+++ Entidades/Cuota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cuota
+    {
+        public long numero { get; set; }
+        public long idPaciente { get; set; }
+        public string tipo { get; set; }    
+        public double salario { get; set; }
+        public double valorServicio { get; set; }
+
+        public void tipoAfiliacion (string tipo)
+        {
+            this.tipo = tipo;
+        }
+    }
+}

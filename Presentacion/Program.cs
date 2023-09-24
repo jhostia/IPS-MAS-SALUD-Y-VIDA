41c11fc4c278054c8573d9b8b081e44d4f31d00d
David Hostia <jhostia@unicesar.edu.co>
2023-09-24 17:29:44 -0500

    Creo una parte del menu de usuario en la capa de presentacion

--- Presentacion/Program.cs
+++ Presentacion/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Datos;
+using Logica;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,23 @@ namespace Presentacion
     {
         static void Main(string[] args)
         {
+            var repo = new LiquidacionCuotaModeradoraRepository(); // Instancia del repositorio
+            var service = new LiquidacionCuotaModeradoraService(repo); // Instancia del servicio
 
-        }
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=== Menú Principal ===");
+                Console.WriteLine("1. Registrar Liquidación");
+                Console.WriteLine("2. Mostrar Todas las Liquidaciones");
+                Console.WriteLine("3. Cantidad de liquidaciones por tipo de afiliacion");
+                Console.WriteLine("4. Valor total de cuotas moderadas y total liquidado por tipo de afiliacion");
+                Console.WriteLine("5. Mostrar liquidaciones por mes y año");
+                Console.WriteLine("6. Eliminar una liquidacion");
+                Console.WriteLine("7. Modificar una liquidacion");
+                Console.WriteLine("8. Salir");
+                Console.WriteLine("");
+                Console.Write("Seleccione una opción: ");
+            }
     }
 }
